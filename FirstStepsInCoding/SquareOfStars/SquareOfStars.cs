caf25c2455855c2657972773df6b8a8ba78c9635
adrian043 <adrian1990@abv.bg>
2017-06-19 13:52:40 +0300

    Revert "Revert "Week 01""
    This reverts commit 6f414cc9f48dd9483ed4d47771acfcb21ba93498.

---
+++ FirstStepsInCoding/SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput01
+{
+    class circle
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            string InitialRows = new String('*', size);
+            string WhiteSpaces = new String(' ', size - 2);
+            Console.WriteLine(InitialRows);
+
+            for (int i = 0; i < size - 2; i = i + 1)
+            {
+                Console.Write('*');
+                Console.Write(WhiteSpaces);
+                Console.Write('*');
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(InitialRows);
+
+        }
+    }
+}
